4429995fc26ec2229118e25c41117393748a5ad6
jearansa <jesus-maria.aransay@unirioja.es>
2021-12-16 00:03:46 +0100

    Introduced definition of beads

---
+++ Beads.thy
@@ -0,0 +1,49 @@
+
+theory Beads
+  imports 
+    Boolean_functions
+begin
+
+definition first_half :: "bool vec \<Rightarrow> bool vec"
+  where "first_half v = vec ((dim_vec v) div 2) (\<lambda> i. (v $ i))"
+
+definition second_half :: "bool vec \<Rightarrow> bool vec"
+  where "second_half v = vec ((dim_vec v) div 2) (\<lambda> i. (v $ (i + ((dim_vec v) div 2))))"
+
+definition bead :: "bool vec => bool"
+  where "bead v = (first_half v = second_half v)"
+
+lemma "bead (vec 4 (\<lambda>x. True))"
+  unfolding bead_def
+  unfolding first_half_def
+  unfolding second_half_def
+  unfolding dim_vec
+  by auto
+
+lemma "bead (vec 8 (\<lambda>x. if x mod 2 = 0 then True else False))"
+  unfolding bead_def
+  unfolding first_half_def
+  unfolding second_half_def by fastforce
+
+lemma "\<not> bead (vec 8 (\<lambda>x. if x = 0 then True else False))"
+(is "\<not> bead ?v")
+proof -
+  have "first_half ?v $ 0 = True"
+    unfolding bead_def
+    unfolding first_half_def by simp
+  moreover have "second_half ?v $ 0 = False"
+    unfolding bead_def
+    unfolding second_half_def by simp
+  ultimately show ?thesis unfolding bead_def by metis
+qed
+
+context boolean_functions
+begin
+
+
+end
+
+
+
+
+end
