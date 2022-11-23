995327ea9214b624ea76d69b5a9ca522078f17a0
jearansa <jesus-maria.aransay@unirioja.es>
2022-11-23 23:43:59 +0100

    Boolean Expressions

--- Binary_Decision_Diagrams.thy
+++ Binary_Decision_Diagrams.thy
@@ -1,7 +1,8 @@
 
 theory Binary_Decision_Diagrams
   imports
-    "ROBDD.Bool_Func"
+    Main
+    "Boolean_Expression_Checkers.Boolean_Expression_Checkers"
 begin
 
 (*datatype ifex = CIF bool | VIF nat | IF ifex ifex ifex*)
@@ -84,8 +85,6 @@ lemma "ifex_unique_var (IF 1 (IF 2 (IF 3 (IF 4 (CIF True) (CIF False))
 lemma "\<not> ifex_unique_var (IF 3 (IF 3 (CIF True) (CIF False))
                          (IF 3 (CIF True) (CIF False)))" by simp
 
-
-
 fun depth :: "ifex \<Rightarrow> nat"
   where 
     "depth (CIF b) = 0" |
@@ -111,7 +110,48 @@ value "reduce (IF 1 (IF 2 (IF 3 (IF 4 (CIF True) (CIF False))
                                  (IF  4 (CIF True) (CIF False))))
                            (IF 4 (CIF True) (CIF False))) (\<lambda>x. if x = 3 then False else True)"
 
+section\<open>Boolean expressions\<close>
+
+text\<open>We are interested in boolean expressions that come from
+  simplicial complexes, that is, collections of simplices
+  that can be expressed as conjunctive normal forms.\<close>
+
+datatype 'a bexp =
+  Const bool |
+  Atom 'a |
+  Neg "'a bexp" |
+  And "'a bexp" "'a bexp" |
+  Or "'a bexp" "'a bexp"
+
+fun bval where
+  "bval (Const b) s = b" |
+  "bval (Atom a) s = s a" |
+  "bval (Neg b) s = (\<not> bval b s)" |
+  "bval (And b1 b2) s = (bval b1 s \<and> bval b2 s)" |
+  "bval (Or b1 b2) s = (bval b1 s \<or> bval b2 s)"
+
+fun bool_expr_of_bexp :: "'a bexp \<Rightarrow> 'a bool_expr" 
+where
+  "bool_expr_of_bexp (Const b) = Const_bool_expr b" 
+  | "bool_expr_of_bexp (Atom a) = Atom_bool_expr a" 
+  | "bool_expr_of_bexp (Neg b) = Neg_bool_expr(bool_expr_of_bexp b)" 
+  | "bool_expr_of_bexp (And b1 b2) = And_bool_expr (bool_expr_of_bexp b1) (bool_expr_of_bexp b2)"
+  | "bool_expr_of_bexp (Or b1 b2) = Or_bool_expr (bool_expr_of_bexp b1) (bool_expr_of_bexp b2)"
+
+lemma val_preservation:
+  "val_bool_expr (bool_expr_of_bexp b) s = bval b s"
+  by (induction b) auto 
+term range
+definition bexp_from_simplice :: "nat set => nat \<Rightarrow> nat bexp"
+  where "bexp_from_simplice \<sigma> n = range
+    (\<lambda>i. if i \<in> \<sigma> then (And i (bexp_from_simplice (\<sigma> - {i})) else (bexp_from_simplice (\<sigma> - {i})) {..<n}"
+
+
+
 lemma
+  
+  
+  
   assumes "depth (IF a t f) = n"
     and "a \<in> vars t"
   shows "True"
